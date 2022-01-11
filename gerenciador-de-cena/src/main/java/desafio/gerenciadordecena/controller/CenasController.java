c3e73755dadcb0ab242bd6f9186d97a90b769a93
rodneyems <rodney.martins@live.com>
2022-01-11 12:44:11 -0300

    add generic endpoint

--- gerenciador-de-cena/src/main/java/desafio/gerenciadordecena/controller/CenasController.java
+++ gerenciador-de-cena/src/main/java/desafio/gerenciadordecena/controller/CenasController.java
@@ -6,17 +6,15 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import desafio.gerenciadordecena.model.DTO.ReturnCenaDto;
 import desafio.gerenciadordecena.model.Models.Cena;
 import desafio.gerenciadordecena.model.Models.StatusEnum;
 
 @RestController
 public class CenasController {
 	@RequestMapping("/cenas")
-	public List<Cena> GetCenas(){
+	public List<ReturnCenaDto> GetCenas(){
 		Cena cena = new Cena(1,"Teste",StatusEnum.Gravada, StatusEnum.Gravada);
-		Cena cena1 = new Cena(2,"Teste 2",StatusEnum.Gravada, StatusEnum.Gravada);
-		Cena cena2 = new Cena(3,"Teste 3",StatusEnum.Gravada, StatusEnum.Gravada);
-		Cena cena3 = new Cena(4,"Teste 4",StatusEnum.Gravada, StatusEnum.Gravada);
-		return Arrays.asList(cena, cena1, cena2, cena3);
+		return ReturnCenaDto.convert(Arrays.asList(cena, cena, cena));
 	}
 }
